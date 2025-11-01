468226dec6f1109343ed4be61d240bb91ceff7b5
Ricardo-Ceia <A49000@alunos.isel.pt>
2025-11-01 16:15:23 +0000

    socket connection between server and client

--- loadserver/loadbalancer.py
+++ loadserver/loadbalancer.py
@@ -13,6 +13,12 @@ class Client(threading.Thread):
         self.id = id
         self.signal = signal
 
+    def __str__(self):
+        return str(self.id) + " " + str(self.address)
+
+    def run(self):
+        pass
+
 
 class Server(threading.Thread):
     def __init__(self, socket, address, id, cpu_usage, signal):
@@ -22,6 +28,18 @@ class Server(threading.Thread):
         self.id = id
         self.cpu_usage = cpu_usage
         self.signal = signal
+
+    def __str__(self):
+        return (
+            str(self.id)
+            + " "
+            + str(self.address)
+            + " current cpu_usage"
+            + str(self.cpu_usage)
+        )
+
+    def run(self):
+        pass
 
 
 class RecHealthPacket:
@@ -41,7 +59,7 @@ def decision(servers: list[Server]) -> int:
     return server_with_smallest_cpu_usage.id
 
 
-def accept_client_connection(socket):
+def new_client_connection(socket):
     while True:
         try:
             sock, address = socket.accept()
@@ -55,15 +73,64 @@ def accept_client_connection(socket):
             break
 
 
-def accept_server_connection(socket):
+def new_server_connection(socket):
     while True:
         try:
             sock, address = socket.accept()
-            global total_client_connections
+            global total_server_connections
             server = Server(sock, address, total_client_connections, 0, True)
             servers.append(server)
             server.start()
-            total_client_connections += 1
+            total_server_connections += 1
         except Exception as e:
             print(f"Error accepting connection: {e}")
             break
+
+
+def main():
+    try:
+        host = input("Host: ")
+        port = int(input("Port: "))
+
+        sock_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock_client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        print(f"Attempting to bind to {host}:{port}...")
+        sock_client.bind((host, port))
+        sock_client.listen(5)
+        sock_server.bind((host, port + 1))
+        sock_server.listen(5)
+        print(f"Server successfully listening on {host}:{port}")
+        print("Waiting for connections...")
+
+        # Create new thread to wait for connections
+        newServerConnectionsThread = threading.Thread(
+            target=new_server_connection, args=(sock_server,)
+        )
+        newClientConnectionsThread = threading.Thread(
+            target=new_client_connection, args=(sock_client,)
+        )
+        newServerConnectionsThread.daemon = False
+        newServerConnectionsThread.start()
+
+        newClientConnectionsThread.daemon = False
+        newClientConnectionsThread.start()
+        # Keep the main thread alive
+        try:
+            while True:
+                pass
+        except KeyboardInterrupt:
+            print("\nServer shutting down...")
+            sock_client.close()
+            sock_server.close()
+            sys.exit(0)
+
+    except Exception as e:
+        print(f"LoadBalancer error: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
